56b18308fd993599cc40d04d0edbf64c11ab2486
Vitor Aoki <vitoraoki17@gmail.com>
2023-05-10 16:19:24 -0300

    requirement 01 - creating migration for the tables users, categories, blog_posts and post_categories

---
+++ src/migrations/20230510175704-create-blog_posts.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('blog_posts', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      content: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        field: 'user_id',
+        references: {
+          model: 'users',
+          key: 'id'
+        }
+      },
+      published: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('blog_posts');
+  }
+};
